ba200b7d77532f9d766b5c2b150f5f95b38c8355
Sajid Saleem Khattak <sajidsaleem707@gmail.com>
2025-08-28 12:53:45 +0500

    hero section created

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <h1 className="heading">Homepage (root)</h1>;
-}
+import React from "react";
+
+const page = () => {
+  return (
+    <section className="pink_container">
+      <h1 className="heading">
+        Pitch Your Start up Ideas <br />
+        Become an entrapraneaur
+      </h1>
+      <p className="sub-heading !max-w-3xl">
+        Submit Ideas, Gets notices and compete with people in virtual
+        competitions
+      </p>
+    </section>
+  );
+};
+
+export default page;
