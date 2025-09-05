6dbdf21d1254133c815fe1bd52efc463ed74a12f
Sajid Saleem Khattak <sajidsaleem707@gmail.com>
2025-09-05 15:29:26 +0500

    font changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,58 +1,58 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import "./globals.css";
 import "easymde/dist/easymde.min.css";
 
-const WorkSans = localFont({
-  src: [
-    {
-      path: "./fonts/workSans-Thin.ttf",
-      weight: "100",
-      style: "normal",
-    },
-    {
-      path: "./fonts/workSans-ExtraLight.ttf",
-      weight: "200",
-      style: "normal",
-    },
-    {
-      path: "./fonts/workSans-Light.ttf",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "./fonts/workSans-Regular.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "./fonts/workSans-Medium.ttf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "./fonts/workSans-SemiBold.ttf",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "./fonts/workSans-Bold.ttf",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "./fonts/workSans-ExtraBold.ttf",
-      weight: "800",
-      style: "normal",
-    },
-    {
-      path: "./fonts/workSans-Black.ttf",
-      weight: "900",
-      style: "normal",
-    },
-  ],
-  variable: "--font-work-sans",
-});
+// const WorkSans = localFont({
+//   src: [
+//     {
+//       path: "./fonts/workSans-Thin.ttf",
+//       weight: "100",
+//       style: "normal",
+//     },
+//     {
+//       path: "./fonts/workSans-ExtraLight.ttf",
+//       weight: "200",
+//       style: "normal",
+//     },
+//     {
+//       path: "./fonts/workSans-Light.ttf",
+//       weight: "300",
+//       style: "normal",
+//     },
+//     {
+//       path: "./fonts/workSans-Regular.ttf",
+//       weight: "400",
+//       style: "normal",
+//     },
+//     {
+//       path: "./fonts/workSans-Medium.ttf",
+//       weight: "500",
+//       style: "normal",
+//     },
+//     {
+//       path: "./fonts/workSans-SemiBold.ttf",
+//       weight: "600",
+//       style: "normal",
+//     },
+//     {
+//       path: "./fonts/workSans-Bold.ttf",
+//       weight: "700",
+//       style: "normal",
+//     },
+//     {
+//       path: "./fonts/workSans-ExtraBold.ttf",
+//       weight: "800",
+//       style: "normal",
+//     },
+//     {
+//       path: "./fonts/workSans-Black.ttf",
+//       weight: "900",
+//       style: "normal",
+//     },
+//   ],
+//   variable: "--font-work-sans",
+// });
 
 export const metadata: Metadata = {
   title: "YC directoy",
